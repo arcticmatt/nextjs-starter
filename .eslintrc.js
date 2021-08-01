002bdd83eda5affe8ecc408ecfcd848dbd9c5a95
Matt Lim <mlimcollege@gmail.com>
2021-08-01 12:42:38 -0700

    Add some more commonly used stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
     "airbnb",
     "prettier",
   ],
+  globals: {
+    JSX: "readonly",
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -23,12 +26,30 @@ module.exports = {
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+
+    camelcase: "off",
+    "func-names": "off",
+    "import/order": "off",
+    "import/no-unresolved": "off",
+    "no-nested-ternary": "off",
+    "no-plusplus": "off",
+    "no-shadow": "off",
+    "no-unused-vars": "off",
 
     "prettier/prettier": "error",
 
+    "react/destructuring-assignment": "off",
     "react/jsx-filename-extension": ["error", { extensions: [".tsx", ".jsx"] }],
     "react/jsx-props-no-spreading": "off",
     "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
     "react-hooks/rules-of-hooks": "error",
   },
 };
