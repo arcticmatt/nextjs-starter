f18c90c838e2b7dd7839b636eb7e7cc785c99f2e
Matt Lim <mlimcollege@gmail.com>
2021-11-06 19:48:25 -0700

    Add colors

---
+++ src/types/enums/BackgroundColorClass.ts
@@ -0,0 +1,24 @@
+enum BackgroundColorClass {
+  BackgroundOverlay = "backgroundColorBackgroundOverlay",
+  Dimmer = "backgroundColorDimmer",
+  Error = "backgroundColorError",
+  Green = "backgroundColorGreen",
+  Ghost = "backgroundColorGhost",
+  LightBlue = "backgroundColorLightBlue",
+  LightPink = "backgroundColorLightPink",
+  LightPurple = "backgroundColorLightPurple",
+  LightPurpleHover = "backgroundColorLightPurpleHover",
+  Navy = "backgroundColorNavy",
+  NavyHover = "backgroundColorNavyHover",
+  Pink = "backgroundColorPink",
+  Primary = "backgroundColorPrimary",
+  Purple = "backgroundColorPurple",
+  Red = "backgroundColorRed",
+  RedHover = "backgroundColorRedHover",
+  Secondary = "backgroundColorSecondary",
+  Tertiary = "backgroundColorTertiary",
+  White = "backgroundColorWhite",
+  Yellow = "backgroundColorYellow",
+}
+
+export default BackgroundColorClass;
---
+++ src/types/enums/ColorClass.ts
@@ -0,0 +1,24 @@
+enum ColorClass {
+  BackgroundOverlay = "colorBackgroundOverlay",
+  Dimmer = "colorDimmer",
+  Error = "colorError",
+  Green = "colorGreen",
+  Ghost = "colorGhost",
+  LightBlue = "colorLightBlue",
+  LightPink = "colorLightPink",
+  LightPurple = "colorLightPurple",
+  LightPurpleHover = "colorLightPurpleHover",
+  Navy = "colorNavy",
+  NavyHover = "colorNavyHover",
+  Pink = "colorPink",
+  Primary = "colorPrimary",
+  Purple = "colorPurple",
+  Red = "colorRed",
+  RedHover = "colorRedHover",
+  Secondary = "colorSecondary",
+  Tertiary = "colorTertiary",
+  White = "colorWhite",
+  Yellow = "colorYellow",
+}
+
+export default ColorClass;
