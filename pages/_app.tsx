490dd358cfc20368831196fa0b97b5bf0a081b7a
Matt Lim <mlimcollege@gmail.com>
2021-11-06 19:48:57 -0700

    Import global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import "@/css/global/Global.css";
 
+// Colors
+import "@/css/global/colors/ColorVariables.css";
+import "@/css/global/colors/BackgroundColorClasses.css";
+import "@/css/global/colors/ColorClasses.css";
+
+// Fonts
+import "@/css/global/fonts/FontClasses.css";
+import "@/css/global/fonts/FontVariables.css";
+
 import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
